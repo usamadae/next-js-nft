1f44981ef4617abf33de7e7c923cac4819b06c98
usama usman <usmanusama4811@gmail.com>
2024-08-08 16:44:27 +0500

    adasd

--- app/components/header.jsx
+++ app/components/header.jsx
@@ -68,7 +68,7 @@ function Header() {
               </button>
             </Link>
             <div onClick={toggleCart} className='cursor-pointer lg:hidden block'>
-              <svg width="40px" height="40px" viewBox="0 -1 20 20" version="1.1">
+              <svg width="20px" height="20px" viewBox="0 -1 20 20" version="1.1">
                 <defs></defs>
                 <g stroke="none" strokeWidth="1" fill="white" fillRule="evenodd">
                   <g transform="translate(-210.000000, -887.000000)" fill="white">
